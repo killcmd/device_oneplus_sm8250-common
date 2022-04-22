b9668eab215449fc4428b2fdb37cbb201cc7c7a8
timocapa <timo.capa@gmail.com>
2022-04-22 17:35:26 +0200

    sm8250-common: Move to EROFS
    Change-Id: Ia3e922bf5ff4207b58c9ee595aca319e8e9ac53a
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- common.mk
+++ common.mk
@@ -8,13 +8,13 @@
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
+    FILESYSTEM_TYPE_vendor=erofs \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
@@ -370,8 +370,7 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/oplus \
-    vendor/ih8sn
+    hardware/oplus
 
 # Telephony
 PRODUCT_PACKAGES += \
